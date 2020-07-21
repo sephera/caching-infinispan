78330cdd89b5821d79a3c34dce6b05b82edf3991
Hai Nguyen <haind.vng@gmail.com>
2020-07-21 20:50:05 +0700

    init project

---
+++ src/main/java/com/example/cachinginfinispan/CachingInfinispanApplication.java
@@ -0,0 +1,13 @@
+package com.example.cachinginfinispan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingInfinispanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CachingInfinispanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cachinginfinispan/CachingInfinispanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cachinginfinispan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingInfinispanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
